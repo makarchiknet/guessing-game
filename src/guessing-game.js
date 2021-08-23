383135f7d819ff60285532c3f54c6d482b663a4f
Kate Makarova <makarchiknet@gmail.com>
2021-08-24 00:29:12 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.try = Math.round((this.maxValue + this.minValue) / 2);
+        return this.try;
     }
 
     lower() {
-
+        this.maxValue = this.try;
     }
 
     greater() {
-
+        this.minValue = this.try;
     }
 }
 
